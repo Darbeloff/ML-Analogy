bf44b77ef1970e19d471319df3a8693cc675728c
rupumped <rupumped@gmail.com>
2020-10-13 19:11:50 -0400

    Maybe data is working again

--- esc.py
+++ esc.py
@@ -18,64 +18,87 @@ import matplotlib.pyplot as plt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dtype = torch.FloatTensor
+torch.manual_seed(6)
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
 N, H = 20, 256
 
-x = []
-y = []
+p_in_data = np.array([])
+spd_data = np.array([])
+eff_data = np.array([])
 with open('esc.csv') as file:
 	reader = csv.DictReader(file)
-	first = True
 	for row in reader:
-		if first:
-			first = False
-		else:
-			x.append([float(row['Input_Power']), float(lastSpeed)])
-			y.append([-float(row['Speed']), -float(row['Efficiency'])])
-		lastSpeed = row['Speed']
+		p_in_data = np.append(p_in_data, float(row['Input_Power']))
+		spd_data = np.append(spd_data, float(row['Speed']))
+		eff_data = np.append(eff_data, float(row['Efficiency']))
 
-x = np.array(x)
-y = np.array(y)
+p_in_data-=np.min(p_in_data)
+spd_data-=np.min(spd_data)
+eff_data-=np.min(eff_data)
+
+p_in_data/=np.max(p_in_data)
+spd_data/=np.max(spd_data)
+eff_data/=np.max(eff_data)
+
+x = np.concatenate((np.transpose(p_in_data[0:-1][None]), np.transpose(spd_data[0:-1][None])),1)
+y = np.concatenate((np.transpose(spd_data[1:][None]), np.transpose(eff_data[1:][None])),1)
 # fig = plt.figure()
 # plt.plot(x[:,0], x[:,1],'r.')
 # plt.show()
 # exit()
 
-x_shift = x.min(axis=0)
-y_shift = y.min(axis=0)
-for i in range(len(x)):
-	x[i]-=x_shift
-	y[i]-=y_shift
-x_transform = x.max(axis=0)
-y_transform = y.max(axis=0)
-for i in range(len(x)):
-	x[i]/=x_transform
-	y[i]/=y_transform
-
-triObj = Triangulation(x[:,0], x[:,1]) 
-plant_s = LinearTriInterpolator(triObj,y[:,0])
-plant_p = LinearTriInterpolator(triObj,y[:,1])
+triObj = Triangulation(x[:,0], x[:,1])
+pre_plant_s = LinearTriInterpolator(triObj,y[:,0])
+pre_plant_p = LinearTriInterpolator(triObj,y[:,1])
+def plant_s(p_in, spd):
+	out = pre_plant_s(p_in, spd)
+	# if np.isnan(out):
+	# 	breakpoint()
+	# 	out = 1
+	return out
+def plant_p(p_in, spd):
+	out = pre_plant_p(p_in, spd)
+	# if np.isnan(out):
+	# 	out = 1
+	return out
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot(x[:,0], x[:,1], y[:,0],'r.')
+ax1 = fig.add_subplot(1,2,1, projection='3d')
+ax1.plot(x[:,0], x[:,1], y[:,1],'r.')
+min_i = np.argmax(y[:,1])
+ax1.plot([x[min_i,0]],[x[min_i,1]],[y[min_i,1]],'g*',label='True Extremum',zorder=1e3)
 plt.xlabel('Input Power')
 plt.ylabel('Speed')
-# # plt.show()
+plt.title('Efficiency')
+ax1.view_init(azim=0, elev=90)
+
+ax2 = fig.add_subplot(1,2,2, projection='3d')
+ax2.plot(x[:,0], x[:,1], y[:,0],'r.')
+min_i = np.argmax(y[:,1])
+ax2.plot([x[min_i,0]],[x[min_i,1]],[y[min_i,1]],'g*',label='True Extremum',zorder=1e3)
+plt.xlabel('Input Power')
+plt.ylabel('Speed')
+plt.title('Next Speed')
+ax2.view_init(azim=0, elev=90)
+
 # xs = np.linspace(0,1,200)
 # ys = np.linspace(0,1,200)
 # xs,ys = np.meshgrid(xs,ys)
 # zs = np.zeros(np.shape(xs))
+# zp = np.zeros(np.shape(xs))
 # for xi in range(np.shape(xs)[0]):
 # 	for yi in range(np.shape(xs)[1]):
-# 		zs[xi,yi] = plant_p(xs[xi,yi],ys[xi,yi])
+# 		zs[xi,yi] = plant_s(xs[xi,yi],ys[xi,yi])
+# 		zp[xi,yi] = plant_p(xs[xi,yi],ys[xi,yi])
 # zs = np.nan_to_num(zs)
-# surf = ax.plot_surface(xs,ys,zs, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-# ax.view_init(azim=0, elev=90)
+# zp = np.nan_to_num(zp)
+# ax1.plot_surface(xs,ys,zp, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+# ax2.plot_surface(xs,ys,zs, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 # plt.show()
 # exit(0)
+
 
 def train_model(x,y):
 	if len(np.shape(y))==1:
@@ -109,7 +132,7 @@ def train_model(x,y):
 	# optimization algorithms. The first argument to the Adam constructor tells the
 	# optimizer which Tensors it should update.
 	learning_rate = .0001
-	n_epochs = 3000
+	n_epochs = 5000
 	training_losses = []
 	validation_losses = []
 	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -134,7 +157,6 @@ def train_model(x,y):
 			model.train()
 			# Forward pass: compute predicted y by passing x to the model.
 			y_pred = model(x_batch)
-			breakpoint()
 
 			# Compute and print loss.
 			loss = loss_fn(y_pred, y_batch)
@@ -160,7 +182,7 @@ def train_model(x,y):
 
 		print(f"[{t+1}] Training loss: {training_loss:.3f}\t Validation loss: {validation_loss:.3f}")
 		
-		if t>1000 and validation_losses[-1]<0.1 and np.mean(validation_losses[-20:-10])<np.mean(validation_losses[-9:-1]):
+		if t>1000 and validation_losses[-1]<0.05 and np.mean(validation_losses[-20:-10])<np.mean(validation_losses[-9:-1]):
 			break
 
 	model.eval()
@@ -181,39 +203,51 @@ model = train_model(x, y)
 xs = np.linspace(0,1,200)
 ys = np.linspace(0,1,200)
 xs,ys = np.meshgrid(xs,ys)
-zs = np.zeros(np.shape(xs))
+z_p = np.zeros(np.shape(xs))
+z_s = np.zeros(np.shape(xs))
 for xi in range(np.shape(xs)[0]):
 	for yi in range(np.shape(xs)[1]):
-		zs[xi,yi] = model(torch.tensor([[xs[xi,yi],ys[xi,yi]]]).type(dtype))[0,0].item()
-zs = np.nan_to_num(zs)
-surf = ax.plot_surface(xs,ys,zs, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+		modelOut = model(torch.tensor([[xs[xi,yi],ys[xi,yi]]]).type(dtype))
+		z_p[xi,yi] = modelOut[0,1].item()
+		z_s[xi,yi] = modelOut[0,0].item()
+ax1.plot_surface(xs,ys,z_p, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+ax2.plot_surface(xs,ys,z_s, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 # plt.show()
 # exit()
 
+DEBUG = False
+# DEBUG = True
+
 t_vec = range(100)
-p_in_vec = [0.3]
-spd_vec = [0.1]
+p_in_vec = [0.8]
+spd_vec = [0.6]
 p_out_vec = []
 p_out_sim = []
 spd_sim = []
+if DEBUG:
+	ax1.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'k+', zorder=1e3)
+	ax2.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'k+', zorder=1e3)
 
 ## Controller
-n_epochs = 200
+n_epochs = 300
 loss_fn = torch.nn.MSELoss(reduction='sum')
 inp = Variable(torch.tensor([[p_in_vec[-1]+0.0, spd_vec[-1]+0.0]]).type(dtype), requires_grad=True)
 gradient_mask = torch.zeros(1,2)
 gradient_mask[0,0] = 1.0
 inp.register_hook(lambda grad: grad.mul_(gradient_mask))
-optimizer = torch.optim.Adam([inp], lr=.02)
+optimizer = torch.optim.Adam([inp], lr=.002)
 for t in range(n_epochs):
 	y0 = model(inp)
-	loss = loss_fn(y0[0,0], torch.tensor([[y0[0,0].item()+0.1]])[0,0])
+	loss = loss_fn(y0[0,1], torch.tensor(y0[0,1].item()+0.1))
 	optimizer.zero_grad()
 	loss.backward()
 	optimizer.step()
+	if DEBUG:
+		ax1.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'g+', zorder=1e3)
+		ax2.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'g+', zorder=1e3)
 
-	p_out_sim.append(y0[0,0].item())
-	spd_sim.append(y0[0,1].item())
+	p_out_sim.append(y0[0,1].item())
+	spd_sim.append(y0[0,0].item())
 	p_in_vec.append(inp[0,0].item())
 	spd_vec.append(plant_s(p_in_vec[-1], spd_vec[-1]).data.item())
 	inp[0,1].data.copy_(torch.tensor(spd_vec[-1]))
@@ -222,7 +256,20 @@ for t in range(n_epochs):
 	# ax.plot([p_in_vec[-1]], [spd_vec[-1]], [p_out_vec[-1]], 'g+')
 	# ax.plot([p_in_vec[-1]], [spd_vec[-1]], [p_out_sim[-1]], 'b+')
 
-	ax.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'k+', zorder=10)
+	if DEBUG:
+		ax1.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'm+', zorder=1e3, label='Steps')
+		ax2.plot([p_in_vec[-1]], [spd_vec[-1]], [1], 'm+', zorder=1e3, label='Steps')
+		plt.show(block=False)
+		breakpoint()
 
-ax.view_init(azim=0, elev=90)
+ax1.plot(p_in_vec, spd_vec, np.ones(np.size(p_in_vec)), 'k+', zorder=1e3, label='Steps')
+ax2.plot(p_in_vec, spd_vec, np.ones(np.size(p_in_vec)), 'k+', zorder=1e3, label='Steps')
+# for loc, spine in ax.spines.items():
+#     # use ax.spines.items() in Python 3
+#     spine.set_linewidth(0)
+# ax.set_xticks([])
+# ax.set_yticks([])
+# ax.set_zticks([])
+plt.tight_layout()
+plt.legend(loc='lower center', ncol=2)
 plt.show()
